d3f2bc1034b83d241a0dfc538c5a82ab434de93c
luckywang1103 <xiaolei_internet@sina.com>
2017-04-09 18:12:18 +0800

    feat: add variable
    add variable j

--- src/rooty.c
+++ src/rooty.c
@@ -2,7 +2,8 @@
 
 uint32_t decrypt_message(const uint8_t *data, uint8_t *decoded_data, uint32_t len, uint8_t *key) {
    uint32_t i;
-
+   uint32_t j;
+   
    for(i = 0; i < len; i++) {
       decoded_data[i] = ((data[i] ^ key[0]) ^ key[1]);
    }
